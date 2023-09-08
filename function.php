533339e98a410e47f334a21d5f10a039f3860109
EHoudon <emmanuel.houdon@le-campus-numerique.fr>
2023-09-08 10:19:36 +0200

    Etape 10

--- function.php
+++ function.php
@@ -7,21 +7,78 @@
 global $song;
 
 
-echo $song['title'];
-echo "<br>";
+function cleanVerseEasy() {
+    global $song;
+
+    $verse = $song['data']['verse'];
+
+    $verse1 = array_merge($verse['first_verse1'], $verse['first_verse2'], $verse['first_verse3']);
+    $verse2 = array_merge($verse['second_verse1'], $verse['second_verse2'], $verse['second_verse3']);
+
+    $song['data']['verse'] = [
+        'first_verse' => $verse1,
+        'second_verse' => $verse2
+    ];
+}
 
 
+function cleanVerseOptimal() {
+    global $song;
+    
+    $newVerse = [
+        'first_verse' => [],
+        'second_verse' => [],
+    ];
 
-    foreach ($song['data'] as $key1 => $paroles) {
-        echo "<br>";
-        
-    foreach ($paroles as $key => $parole) {
-        if ($key%2 === 1 && $key1 != "chorus_1" && $key1 != "chorus_2" ){
-        continue;
+    // On parcours le tableau afin de fusionner les parties de couplets
+    foreach($song['data']['verse'] as $key => $verse) {
+
+        if( str_contains( $key , 'first' ) ) {
+            $newVerse['first_verse'] = array_merge( $newVerse['first_verse'], $verse );
         }
-        echo "$parole <br>";
+        if( str_contains( $key, 'second' ) ) {
+            $newVerse['second_verse'] = array_merge( $newVerse['second_verse'], $verse );
+        }
+    }
+
+    // On écrase l'ancien tableau par le nouveau
+    $song['data']['verse'] = $newVerse;
+}
+
+function getAuthor() {
+    global $song;
+    return '<br>' . $song['author'];
+}
+
+function getChorus() {
+    global $song;
+
+    $html = '<br>';
+    foreach ($song['data']['chorus'] as $line) {
+        $html .= $line . '<br>';
+    }
+    $html .= '<br>';
+
+    return $html;
+}
+
+function getTitle() {
+    global $song;
+    return $song['title'] . '<br>';
+}
+
+// Nettoyage
+cleanVerseOptimal();
+
+// Affichage
+echo getTitle();
+
+foreach ($song['data']['verse'] as $verse) {
+    echo getChorus();
+    
+    foreach ($verse as $line) {
+        echo $line . '<br>';
     }
 }
 
-echo "<br>";
-echo $song['author'];
+echo getAuthor();
